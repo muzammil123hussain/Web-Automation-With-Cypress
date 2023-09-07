a194ad3fadd1872c1654cc12ba562c7650ace436
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-09-07 23:45:33 +0500

    first test complete

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      watchForFileChanges : false
+      return {
+        watchForFileChanges: false,
+      };
     },
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,14 @@ describe("Open TODO and add element and click on it", () => {
   it("passes", () => {
     cy.visit("http://todomvc-app-for-testing.surge.sh/");
     cy.get(".new-todo").type("learn Cypress{enter}");
+
+    cy.get("label").should("have.text", "learn Cypress");
+    cy.get(".toggle").should("not.be.checked");
+
     cy.get(".toggle").click();
+    cy.get("label").should("have.css", "text-decoration-line", "line-through");
+
     cy.contains("Clear").click();
+    cy.get(".new-todo").should("not.have.descendants", "li");
   });
 });
