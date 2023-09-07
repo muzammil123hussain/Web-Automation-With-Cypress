2ed7a818600638529a9bbef14a2f5fd13b1b2b69
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-09-07 23:27:08 +0500

    add more assert

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/')
-  })
-})
+describe("Open TODO and add element and click on it", () => {
+  it("passes", () => {
+    cy.visit("http://todomvc-app-for-testing.surge.sh/");
+    cy.get(".new-todo").type("learn Cypress{enter}");
+    cy.get(".toggle").click();
+    cy.contains("Clear").click();
+  });
+});
