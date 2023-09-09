a61eeca0e8891aadd7ff715f53dfa0f5e3605931
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-09-09 15:17:51 +0500

    filter test added

--- cypress/e2e/filter.cy.js
+++ cypress/e2e/filter.cy.js
@@ -1,5 +1,7 @@
+/// <reference types="Cypress" />
+
 describe("TODO Filter Test Cases", () => {
-  it("Add TODO items", () => {
+  beforeEach(() => {
     cy.visit("http://todomvc-app-for-testing.surge.sh/");
 
     cy.get(".new-todo").type("Learn JS{enter}");
@@ -16,6 +18,14 @@ describe("TODO Filter Test Cases", () => {
 
   it("Filer Active TODO items", () => {
     cy.contains("Active").click();
-    cy.get(".todi-list li");
+    cy.get(".todo-list li").should("have.length", 2);
+  });
+  it("Filer Completed TODO items", () => {
+    cy.contains("Completed").click();
+    cy.get(".todo-list li").should("have.length", 1);
+  });
+  it("Filer All TODO items", () => {
+    cy.contains("All").click();
+    cy.get(".todo-list li").should("have.length", 3);
   });
 });
