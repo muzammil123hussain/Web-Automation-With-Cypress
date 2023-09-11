bdba78c9a50ed017dc7c210b6e625d3204d969d5
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2023-09-11 15:18:00 +0500

    imp

--- cypress/page-objects/todo-page.js
+++ cypress/page-objects/todo-page.js
@@ -4,7 +4,7 @@ export class TodoPage {
   navigate() {
     cy.visit("http://todomvc-app-for-testing.surge.sh/");
   }
-  addTodo(todotxt) {
-    cy.get(".new-todo").type(todotxt + "{enter}");
+  addTodo(todotext) {
+    cy.get(".new-todo").type(todotext + "{enter}");
   }
 }
